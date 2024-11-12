0bbbb860654c4c9bef8a21fedd85f08d4f05655e
Pedro Henrique Correia de Oliveira <149112597+PedroHCorreia@users.noreply.github.com>
2024-11-12 15:59:28 -0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -7,6 +7,9 @@
 int main() {
     //Somente para testes da lista//
     Lista *lista = criaLista();
+    if(carregarRegistros(lista)){
+        printf("Qtde de Regs: %d", lista->qtde);
+    }
     ABB *arvIdade = criaABB();
     ABB *arvAno = criaABB();
     ABB *arvMes = criaABB();
@@ -32,8 +35,10 @@ int main() {
 
         switch(opcao) {
             case 1: {
+                printf("Erro 6");
                 Registro *novoRegistro = criaRegistro();
-                inserirLista(lista, novoRegistro);
+                printf("Erro 7");
+                Cadastrar_novo_paciente(lista, novoRegistro);
                 insereABBGeral(arvIdade,arvAno,arvMes,arvDia, novoRegistro);
                 printf("Paciente cadastrado com sucesso!\n");
                 break;
